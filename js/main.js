e85e5d755a0b6331fce9e4d380364b519bf3203e
hellocreep <hellocreep@gmail.com>
2013-12-05 17:42:03 +0800

    .

--- js/main.js
+++ js/main.js
@@ -27,53 +27,6 @@ require([
 	'theme_maker'
 ],function(ThemeMaker) {
 
-	// $('.panel-group h4').on('click', function() {
-	// 	$(this).parent().addClass('active').siblings().removeClass('active');
-	// });
-
-	// $('.skin-panel-trigger').click(function() {
-	// 	var offset = '-300px',
-	// 		$this = $(this);
-	// 	if($this.hasClass('hide-panel')) offset = 0;
-	// 	$('.skin-panel').animate({
-	// 		left: offset 
-	// 	}, 800, function() {
-	// 		$this.toggleClass('hide-panel');
-	// 	});
-	// });
-
-	// $('#picker').colpick();
-
-	// $('.skin-width').noUiSlider({
-	// 	range: [0, 1500],
-	// 	start: 1000,
-	// 	step: 10,
-	// 	handles: 1,
-	// 	slide: function() {
-	// 		$('.skin-width-val').text($('.skin-width').val()*1 + 'px');
-	// 	}
-	// });
-
-	// $('.content-width').noUiSlider({
-	// 	range: [0, 1400],
-	// 	start: 1000,
-	// 	step: 10,
-	// 	handles: 1,
-	// 	slide: function() {
-	// 		$('.content-width-val').text($('.content-width').val()*1 + 'px');
-	// 	}
-	// });
-
-	// $('.header-height').noUiSlider({
-	// 	range: [0, 170],
-	// 	start: 100,
-	// 	step: 10,
-	// 	handles: 1,
-	// 	slide: function() {
-	// 		$('.header-height-val').text($('.header-height').val()*1 + 'px');
-	// 	}
-	// });
-
 	var themeMaker = new ThemeMaker({
 		backgroundImage: {
 			effectedTarget: []
@@ -115,6 +68,5 @@ require([
 			start: 100
 		}
 	});
-
 
 });
